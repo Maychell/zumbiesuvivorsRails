fc0acbff327a3ec4ed97276d5c84345c8956343a
Maychell <maychellfernandes@hotmail.com>
2016-09-16 14:28:45 +0100

    indentacao

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,15 +1,15 @@
 class ComplaintsController < ApplicationController
-	include Trailblazer::Operation::Controller
-	skip_before_action :verify_authenticity_token
+  include Trailblazer::Operation::Controller
+  skip_before_action :verify_authenticity_token
 
   respond_to :html, :json
 
-	def new
-		form Complaint::Create
-	end
+  def new
+    form Complaint::Create
+  end
 
-	def create
-		respond Complaint::Create do |op, format|
+  def create
+    respond Complaint::Create do |op, format|
       if op.valid?
         format.html { redirect_to action: :new }
         format.json { render json: op.model }
@@ -18,5 +18,5 @@ class ComplaintsController < ApplicationController
         format.json { render json: op.errors, status: :unprocessable_entity }
       end
     end
-	end
+  end
 end
